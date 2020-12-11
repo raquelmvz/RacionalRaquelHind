da86a913463251794b29ad3098f7ad367125e869
raquelmvz <raquel.mvz98@gmail.com>
2020-12-11 18:41:25 +0100

    Crea clase prueba y dos racionales

---
+++ src/main/java/racionalRaquelHind/AppRacional.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package racionalRaquelHind;
+
+/**
+ *
+ * @author raque
+ */
+public class AppRacional {
+    
+    public static void main(String[] args) {
+        
+        //Creacion de dos racionales con valores 4/5 y 3/4
+        
+        Racional z = new Racional(4,5);
+        
+        Racional k = new Racional(3,4);
+        
+        
+    }
+    
+}
